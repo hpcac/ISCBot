d71dcee8d265df115cf58f924b2241e234865c69
JanLJL <jan.laukemann@fau.de>
2024-04-04 09:57:57 -0700

    changed TelegramBot API for v21.0.1

--- iscbot/iscbot.py
+++ iscbot/iscbot.py
@@ -5,10 +5,11 @@ from datetime import datetime
 from functools import wraps
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram import ParseMode, ReplyKeyboardMarkup
-from telegram.ext import CallbackQueryHandler
-from telegram.ext import CommandHandler, Filters, MessageHandler
-from telegram.ext import Updater
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.constants import ParseMode
+from telegram.ext import CallbackQueryHandler, ContextTypes
+from telegram.ext import CommandHandler, filters, MessageHandler
+from telegram.ext import Application
 
 from backend import Backend
 
@@ -44,80 +45,85 @@ class ISCBot(object):
         # Initialize logging
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
+        # set higher logging level for httpx to avoid all GET and POST requests being logged
+        logging.getLogger("httpx").setLevel(logging.WARNING)
         # Start backend
         self.pdus = Backend()
         print('Successfully initialized backend!')
 
         # Start Bot
-        self.updater = Updater(token='565616615:AAHeVav01akOO_ox2RLED8jdLqMISLL-Hfc')
-        dispatcher = self.updater.dispatcher
-        self.queue = self.updater.job_queue
+        self.application = Application.builder().token('565616615:AAHeVav01akOO_ox2RLED8jdLqMISLL-Hfc').build()
+        app = self.application
+        self.queue = self.application.job_queue
 
         # Add handler
         start_handler = CommandHandler('start', self.start)
-        dispatcher.add_handler(start_handler)
+        app.add_handler(start_handler)
 
         help_handler = CommandHandler('help', self.get_help)
-        dispatcher.add_handler(help_handler)
+        app.add_handler(help_handler)
 
         #grp_handler = CommandHandler('grp', self.send_group)
-        #dispatcher.add_handler(grp_handler)
+        #app.add_handler(grp_handler)
 
         curr_power_handler = CommandHandler('current', self.current)
-        dispatcher.add_handler(curr_power_handler)
+        app.add_handler(curr_power_handler)
 
         peak_power_handler = CommandHandler('peaks', self.peaks)
-        dispatcher.add_handler(peak_power_handler)
+        app.add_handler(peak_power_handler)
 
         peak_dates_handler = CommandHandler('peakdates', self.peak_dates)
-        dispatcher.add_handler(peak_dates_handler)
+        app.add_handler(peak_dates_handler)
 
         #reset_handler = CommandHandler('reset', self.reset_pdu)
-        #dispatcher.add_handler(reset_handler)
+        #app.add_handler(reset_handler)
 
         inline_handler = CommandHandler('reset', self.reset_pdu_inline)
-        dispatcher.add_handler(inline_handler)
+        app.add_handler(inline_handler)
 
         cbquery_handler = CallbackQueryHandler(self.cb_query)#, pass_groups=True)
-        dispatcher.add_handler(cbquery_handler)
+        app.add_handler(cbquery_handler)
 
         # Unknown commands
-        unknw_handler = MessageHandler(Filters.command, self.unknown)
-        dispatcher.add_handler(unknw_handler)
+        unknw_handler = MessageHandler(filters.COMMAND, self.unknown)
+        app.add_handler(unknw_handler)
 
     #-------Permission Config--------#
     def restricted(func):
         @wraps(func)
-        def wrapped(self, bot, update, *args, **kwargs):
+        def wrapped(self, update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs):
             user_id = update.effective_user.id
             chat_id = update.effective_message.chat.id
             if user_id not in self.access_list and chat_id != self.ISC_grpID:
                 print("Unauthorized access denied for {}.".format(user_id))
                 return
-            return func(self, bot, update, *args, **kwargs)
+            return func(self, update, context, *args, **kwargs)
         return wrapped
 
     #---------Main functions---------#
-    def current(self, bot, update):
+    async def current(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Gets current power usage of all teams from backend and sends it to the user.
         """
-        bot.send_message(chat_id=update.message.chat_id, text=self.pdus.current())
+        await update.message.reply_text(text=self.pdus.current())
 
-    def peaks(self, bot, update):
+
+    async def peaks(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Gets the peak power values of all teams from backend and sends it to the user.
         """
-        bot.send_message(chat_id=update.message.chat_id, text=self.pdus.peaks())
+        await update.message.reply_text(text=self.pdus.peaks())
 
-    def peak_dates(self, bot, update):
+    
+    async def peak_dates(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Gets the peak power values of all teams with corresponding timestamps from backend
         and sends it to the user.
         """
-        bot.send_message(chat_id=update.message.chat_id, text=self.pdus.peak_dates())
+        await update.message.reply_text(text=self.pdus.peak_dates())
 
-    def check_limits(self, bot, job):
+
+    async def check_limits(self, context: ContextTypes.DEFAULT_TYPE):
         """
         Gets list of all teams off the power limit from the backend and sends push notifications.
         Possible ways for sending it: a) First user in access list, b) in the group.
@@ -125,12 +131,13 @@ class ISCBot(object):
         exceeders, not_reachable = self.pdus.check_exceedings()
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n'
         self.counter += 1
+        bot = context.bot
         if(len(exceeders) > 0):
             for ex in exceeders:
                 print(now + ex)
                 #for rcv in self.access_list:
                 #    bot.send_message(chat_id=rcv, text=ex)
-                bot.send_message(chat_id=self.access_list[0], text=ex)
+                await bot.send_message(chat_id=self.access_list[0], text=ex)
                 #bot.send_message(chat_id=self.access_list[1], text=ex)
 
                 #Additionally, log in file
@@ -153,13 +160,13 @@ class ISCBot(object):
 
 
     @restricted
-    def reset_pdu_inline(self, bot, update):
+    async def reset_pdu_inline(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Resets the peak power of PDU for certain IP address via new inline KeyboardMarkup.
         """
         txt = 'Please choose the team to reset their PDU: '
         markup = InlineKeyboardMarkup(inline_keyboard=self.create_inline_keyboard())
-        bot.send_message(chat_id=update.message.chat_id, text=txt, reply_markup=markup)
+        await update.message.reply_text(text=txt, reply_markup=markup)
 
     #--------------------------------#
 
@@ -169,16 +176,16 @@ class ISCBot(object):
         bot.send_message(chat_id=self.ISC_grpID, text='Hello Group from {}'.format(
                          update.message.from_user.first_name))
 
-    def start(self, bot, update):
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Starting point for everybody texting ISCBot for the first time. Start via /start.
         """
-        bot.send_message(chat_id=update.message.chat_id, text=('Hi there! I am the ISC PDU Bot '
+        await update.message.reply_text(text=('Hi there! I am the ISC PDU Bot '
                          + 'and can help you monitoring the PDUs. Type in your commands '
                          + 'with a \'/\' in the beginning.'))
 
 
-    def get_help(self, bot, update):
+    async def get_help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         List of all commands and brief explanations. Start via /help.
         """
@@ -188,30 +195,31 @@ class ISCBot(object):
             for line in f:
                 helptext += line
 
-        bot.send_message(chat_id=update.message.chat_id, text=helptext,
-                         parse_mode=ParseMode.MARKDOWN)
+        await update.message.reply_text(text=helptext, parse_mode=ParseMode.MARKDOWN_V2)
 
-    def unknown(self, bot, update):
+
+    async def unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Default output for invalid commands or other messages in private chat.
         """
-        update.message.reply_text('Sorry, I didn\'t understand that command.')
+        await update.message.reply_text('Sorry, I didn\'t understand that command.')
 
 
     @restricted
-    def cb_query(self, bot, update):
+    async def cb_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
         Helper function for resetting the Rack PDUs.
         """
-        team = update.callback_query.data
-        bot.answer_callback_query(callback_query_id=update.callback_query.id,
+        query = update.callback_query
+        team = query.data
+        await context.bot.answer_callback_query(callback_query_id=query.id,
                                   text=('Resetting PDU of {}. '
-                                       + 'Please wait...').format(team))
+                                       + 'Please wait until the bot replies...').format(team))
         if(self.pdus.reset(team)):
             ips = ", ".join([str(ip) for ip in sorted(self.pdus.teams[team].keys())])
-            self.edit_message_text_wrapper(bot,
-                                  update.callback_query.message.chat_id,
-                                  update.callback_query.message.message_id,
+            await self.edit_message_text_wrapper(context.bot,
+                                  query.message.chat_id,
+                                  query.message.message_id,
                                   ('Peak Power of PDU of team {} ({}) successfully '
                                   + 'reset.').format(team, ips))
             #Additionally, log in file
@@ -221,15 +229,15 @@ class ISCBot(object):
                 f.write(now + ' --- ' + ex)
             return True
         else:
-            self.edit_message_text_wrapper(bot,
-                                  update.callback_query.message.chat_id,
-                                  update.callback_query.message.message_id,
+            await self.edit_message_text_wrapper(context.bot,
+                                  query.message.chat_id,
+                                  query.message.message_id,
                                   ('Something went wrong during resetting PDU of team {} ({}). You '
                                   + 'might want to try it again!').format(team, ips))
             return False
 
     # Avoid telegram.error.BadRequest
-    def edit_message_text_wrapper(self, bot, chat_id, message_id, text):
+    async def edit_message_text_wrapper(self, bot, chat_id, message_id, text):
         if ('chat_id' not in self.last_msg
             or self.last_msg['chat_id'] != chat_id
             or self.last_msg['message_id'] != message_id
@@ -239,7 +247,7 @@ class ISCBot(object):
             self.last_msg['chat_id'] = chat_id
             self.last_msg['message_id'] = message_id
             self.last_msg['text'] = text
-            bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
+            await bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
             return
         else:
             # Nothing to do
@@ -267,12 +275,15 @@ class ISCBot(object):
 #-------Main method-------#
 
 def main():
-    VERSION = '1.4'
+    VERSION = '1.5'
     print('Start ISCBot v{}'.format(VERSION))
     iscbot = ISCBot()
+    # Create repeating check for power limits
+    check_job = iscbot.queue.run_repeating(iscbot.check_limits, interval=2, first=0)
+    # disable logging of running check job
+    logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
     # Start polling
-    iscbot.queue.run_repeating(iscbot.check_limits, interval=2, first=0)
-    iscbot.updater.start_polling()
+    iscbot.application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 #-------Main method-------#
